ee8a582fae82371de96fee75735a6f80b68e679c
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-10-06 00:32:34 +0300

    done fourth task

--- Seminars/FirstLesson/FourthTask.java
+++ Seminars/FirstLesson/FourthTask.java
@@ -1,5 +1,19 @@
 package Seminars.FirstLesson;
 
 public class FourthTask {
-    
+    public static void main(String[] args) {
+        Integer[] array = new Integer[]{1, null, 13, null, null, 20};
+        checkArray(array);
+        }
+    public static void checkArray(Integer[] arr){
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i] == null){
+                sb.append(i).append(" ");
+            }
+        }
+        if(sb.length() != 0){
+            throw new RuntimeException(sb.toString());
+        }
+    }
 }
