4ecdd9c18444beab0dd2e75ca2e3d5808cd3656b
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-10-05 23:39:41 +0300

    done SecondTask in Seminars

--- Seminars/FirstLesson/SecondTask.java
+++ Seminars/FirstLesson/SecondTask.java
@@ -1,5 +1,40 @@
 package Seminars.FirstLesson;
 
 public class SecondTask {
-    
+    public static void main(String[] args) {
+        // int[] array = new int[]{1, 6, 13, 16, 19, 20};
+        // int[] array = new int[]{};
+        int[] array = null;
+        mehtod2(array, 3);
+    }
+
+    public static int createArray(int[] array, int a){
+        if (array == null){
+                return -3;
+            }
+         int buf = array.length;
+        if (buf < 1){
+            return -1;
+        }
+        for (int i = 0; i < array.length; i++) {
+            if(array[i] == a){
+                return i;
+            }
+        }
+        return -2;
+    }
+
+    public static void mehtod2(int[] array, int a){
+        int buf = createArray(array, a);
+        if (buf == -1){
+            System.out.println("Длинна массива меньше " + 
+                                "заданного минимума.");
+        }
+        else if (buf == -2){
+            System.out.println("Искомый элемент не найден.");
+        } 
+        else if (buf == -3){
+            System.out.println("Массив не имеет значений.");
+        }
+    }
 }
