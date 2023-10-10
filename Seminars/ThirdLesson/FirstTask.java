a458bbe88a42111497e2f5a12424299ce4e8c2b3
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-10-10 14:22:58 +0300

    done the first task of the third seminar lesson

--- Seminars/ThirdLesson/FirstTask.java
+++ Seminars/ThirdLesson/FirstTask.java
@@ -1,6 +1,17 @@
 package Seminars.ThirdLesson;
 
+import java.io.IOException;
+
 public class FirstTask {
 
-    
+    public static void main(String[] args) {
+        try {
+            doSomething();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+    public static void doSomething() throws IOException{
+
+    }
 }
