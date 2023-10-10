dcb9d90dc53b9e1977880744e9a11a33fa5d0dc8
Daniil 'Creator_my_life' Koblov <1koblov.danil1@gmail.com>
2023-10-10 14:42:16 +0300

    done the second task of the third seminar lesson

--- Seminars/ThirdLesson/SecondTask/Counter.java
+++ Seminars/ThirdLesson/SecondTask/Counter.java
@@ -1,5 +1,37 @@
 package Seminars.ThirdLesson.SecondTask;
 
-public class Counter {
-    
+import java.io.IOException;
+
+public class Counter implements AutoCloseable{
+        private Integer count;
+
+        public Counter(int count){
+            this.count = count;
+        }
+        public int getCount() throws IOException {
+            hasClose();
+            return count;
+        }
+
+        public void setCount(int count) throws IOException {
+            hasClose();
+            this.count = count;
+        }
+
+        public void add () throws IOException{
+            hasClose();
+            this.count ++;
+        }
+
+        @Override
+        public void close() throws Exception {
+            this.count = null;
+        }
+        
+        public void hasClose() throws IOException{
+            if (count == null){
+                throw new IOException("Экземпляр закрыт.");
+            }
+
+        }
 }
--- Seminars/ThirdLesson/SecondTask/Main.java
+++ Seminars/ThirdLesson/SecondTask/Main.java
@@ -1,5 +1,15 @@
 package Seminars.ThirdLesson.SecondTask;
 
 public class Main {
-    
+    public static void main(String[] args) {
+
+        try (Counter counter = new Counter(2)){
+            counter.add();
+            counter.close();
+            counter.add();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
